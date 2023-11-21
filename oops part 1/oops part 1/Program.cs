23658268ecc24e186e0eb3f0dfca90db23bdd507
Jambukesh <jkesh360@gmail.com>
2023-11-21 18:22:56 +0530

    day12

---
+++ oops part 1/oops part 1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oops_part_1
+{
+    internal class Program
+
+    {
+
+        static void Main(string[] args)
+        {
+            BankAccount myAccount = new BankAccount("jambu");
+            myAccount.Deposit(100000);
+            myAccount.Withdraw(500);
+            Console.WriteLine($"account Number:{myAccount.AccountNumber}");
+            Console.WriteLine($"account Holder:{myAccount.AccountHolderName}");
+            Console.WriteLine($"account balance:{myAccount.Balance}");
+            Console.ReadKey();
+
+
+        }
+    }
+}
